c131b3a9285b15fb1c7d056f9944c5bdef7b90dc
WhoAmI <bobir_1997@mail.ru>
2019-05-12 10:21:58 +0500

    Added uzbek (latin) language option

---
+++ src/langs/uz.js
@@ -0,0 +1,61 @@
+/* ===========================================================
+ * ru.js
+ * Russion translation for Trumbowyg
+ * http://alex-d.github.com/Trumbowyg
+ * ===========================================================
+ * Author : Yuri Lya
+ */
+
+jQuery.trumbowyg.langs.uz = {
+    viewHTML: 'HTML formatda ko\'rish',
+
+    undo: 'Qaytarish',
+    redo: 'Takrorlash',
+
+    formatting: 'Formatlashtirish',
+    p: 'Oddiy',
+    blockquote: 'Iqtibos',
+    code: 'Kod',
+    header: 'Sarlavha',
+
+    bold: 'Qalin',
+    italic: 'Kursiv',
+    strikethrough: 'Chizib tashlangan',
+    underline: 'Belgilangan',
+
+    strong: 'Qalin',
+    em: 'Kursiv',
+    del: 'Chizib tashlangan',
+
+    superscript: 'Satr usti',
+    subscript: 'Satr osti',
+
+    unorderedList: 'Oddiy ro\'yxat',
+    orderedList: ' Raqamlangan ro\'yxat ',
+
+    insertImage: 'Rasm qo\'yish',
+    insertVideo: 'Video qo\'yish',
+    link: 'Manzil',
+    createLink: 'Manzil yaratish',
+    unlink: 'Manzil o\'chirish',
+
+    justifyLeft: 'Chapga saflash',
+    justifyCenter: 'O\'rtaga saflash',
+    justifyRight: 'O\'nga saflash',
+    justifyFull: 'Eniga saflash',
+
+    horizontalRule: 'Gorizontal chiziq',
+    removeformat: 'Formatni o\'chirish',
+
+    fullscreen: 'To\'liq ekran',
+
+    close: 'Yopish',
+
+    submit: 'Qo\'yish',
+    reset: 'Bekor qilish',
+
+    required: 'Shartli',
+    description: 'Ta\'rif',
+    title: 'Maslahat',
+    text: 'Matn'
+};
